0c543ab98478d073aaaa285cd9427dfc7c8feccb
MaamounGrissa <grissa.maamoun@gmail.com>
2019-10-23 18:35:00 +0300

    Delete mail Parameters

--- mail.php
+++ mail.php
@@ -27,16 +27,16 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                       // Enable verbose debug output
     $mail->isSMTP();                                             // Send using SMTP
-    $mail->Host       = 'smtp.elasticemail.com';                 // Set the SMTP server to send through
+    $mail->Host       = 'YourSMTP Here';                 // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                    // Enable SMTP authentication
-    $mail->Username   = 'grissa.maamoun@gmail.com';              // SMTP username
-    $mail->Password   = '5b8a5a39-e442-46f5-b1f7-27818f4e3359';  // SMTP password
+    $mail->Username   = 'UserName';              // SMTP username
+    $mail->Password   = 'Password';  // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 2525;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('grissa.maamoun@gmail.com', $email);
-    $mail->addAddress('grissa.maamoun@gmail.com', 'Maamoun Grissa');     // Add a recipient
+    $mail->setFrom($email, $email);
+    $mail->addAddress('Adress recipient here', 'Your Name');     // Add a recipient
     // $mail->addAddress('ellen@example.com');                           // Name is optional
     // $mail->addReplyTo('info@example.com', 'Information');
     // $mail->addCC('cc@example.com');
